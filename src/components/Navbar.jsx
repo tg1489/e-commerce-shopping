d2c4fea7f396ee251e899d09ce607640f4dde123
Tony <tonyguarino1489@gmail.com>
2024-02-04 12:03:43 -0500

    Added styled wrapper comp to navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,22 @@
 import React from 'react';
+import '../css/Navbar.css';
 import { styled } from 'styled-components';
 
+// Styled-Components
 const Container = styled.div`
   height: 60px;
-  background-color: #000;
+`;
+
+const Wrapper = styled.div`
+  padding: 10px 20px;
 `;
 
 export default function Navbar() {
-  return <Container>Navbar</Container>;
+  return (
+    <>
+      <Container>
+        <Wrapper>Navbar</Wrapper>
+      </Container>
+    </>
+  );
 }
