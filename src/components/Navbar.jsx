5fd6316bf7248c5312135c0135d1eb6eda6eb8bf
Tony <tonyguarino1489@gmail.com>
2024-02-04 11:49:43 -0500

    added new container to navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
+import { styled } from 'styled-components';
 
-// const 
+const Container = styled.div`
+  height: 60px;
+  background-color: #000;
+`;
 
 export default function Navbar() {
-  return <div>Navbar</div>;
+  return <Container>Navbar</Container>;
 }
