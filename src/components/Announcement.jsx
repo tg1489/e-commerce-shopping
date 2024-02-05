30b6534c1601efade0741e59bea37b9c7fd5267b
Tony <tonyguarino1489@gmail.com>
2024-02-05 15:35:02 -0500

    added style and example content to announcement

---
+++ src/components/Announcement.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { styled } from 'styled-components';
+
+const Container = styled.div`
+  height: 30px;
+  background-color: teal;
+  color: white;
+`;
+
+export default function Announcement() {
+  return <Container>Super Deal! Free Shipping on Orders Over $50</Container>;
+}
