27c79cfc64ad148dbec0a3dd552325d3eb27f2d3
Tony <tonyguarino1489@gmail.com>
2024-02-06 15:21:41 -0500

    added arrow component and styled container

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -1,7 +1,18 @@
 import React from 'react'
+import { styled } from 'styled-components'
+
+const Container = styled.div`
+    width: 100%;
+    height: 100vh;
+    display: flex;
+`
 
 export default function Slider() {
   return (
-    <div>Slider</div>
+    <Container>
+        <Arrow>
+            
+        </Arrow>
+    </Container>
   )
 }
