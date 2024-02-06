049fa1915a788b6fcf64701afc4b34655ee7e547
Tony <tonyguarino1489@gmail.com>
2024-02-06 15:26:41 -0500

    Slider styles and arrows added

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -1,18 +1,31 @@
-import React from 'react'
-import { styled } from 'styled-components'
+import React from 'react';
+import { styled } from 'styled-components';
+import ArrowLeftOutlinedIcon from '@mui/icons-material/ArrowLeftOutlined';
+import { ArrowRightOutlined } from '@mui/icons-material';
 
 const Container = styled.div`
-    width: 100%;
-    height: 100vh;
-    display: flex;
-`
+  width: 100%;
+  height: 100vh;
+  display: flex;
+`;
+
+const Arrow = styled.div`
+  width: 50px;
+  height: 50px;
+  background-color: #fff7f7;
+  border-radius: 50%;
+`;
 
 export default function Slider() {
   return (
     <Container>
-        <Arrow>
-            
-        </Arrow>
+      <Arrow>
+        <ArrowLeftOutlinedIcon />
+      </Arrow>
+
+      <Arrow>
+        <ArrowRightOutlined />
+      </Arrow>
     </Container>
-  )
+  );
 }
