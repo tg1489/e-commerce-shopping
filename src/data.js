6c8470e81944a42f34221c530f3dd384a58e4ce0
Tony <tonyguarino1489@gmail.com>
2024-02-24 10:02:46 -0500

    added data file and objects inside

---
+++ src/data.js
@@ -0,0 +1,25 @@
+import shopping from './assets/shopping.jpg';
+
+export const sliderItems = [
+  {
+    id: 1,
+    img: shopping,
+    title: 'SUMMER SALE',
+    desc: `DON'T COMPROMISE ON STYLE! GET A FLAT 30% OFF FOR NEW ARRIVALS`,
+    bg: 'f5fafd',
+  },
+  {
+    id: 2,
+    img: shopping,
+    title: 'WINTER SALE',
+    desc: `DON'T COMPROMISE ON STYLE! GET A FLAT 30% OFF FOR NEW ARRIVALS`,
+    bg: 'fcf1ed',
+  },
+  {
+    id: 3,
+    img: shopping,
+    title: 'POPULAR SALE',
+    desc: `DON'T COMPROMISE ON STYLE! GET A FLAT 30% OFF FOR NEW ARRIVALS`,
+    bg: 'fbf0f4',
+  },
+];
