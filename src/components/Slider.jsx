87eee65c2a37e4f8481898dab4915af8b5562010
Tony <tonyguarino1489@gmail.com>
2024-02-24 10:29:42 -0500

    added function data for slide

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -82,7 +82,13 @@ const Button = styled.button`
 
 export default function Slider() {
   const [slideIndex, setSlideIndex] = useState(0);
-  const handleClick = (direction) => {};
+  const handleClick = (direction) => {
+    if (direction === 'left') {
+      setSlideIndex(slideIndex > 0 ? slideIndex - 1 : 2);
+    } else {
+      setSlideIndex(slideIndex < 2 ? slideIndex + 1 : 0);
+    }
+  };
 
   return (
     <Container>
