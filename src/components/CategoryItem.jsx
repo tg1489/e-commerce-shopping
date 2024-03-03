1687b168a73f384b3f80d01c592f34bc1fd50cf1
Tony <tonyguarino1489@gmail.com>
2024-03-03 10:44:56 -0500

    Added styles to fix images at bottom

--- src/components/CategoryItem.jsx
+++ src/components/CategoryItem.jsx
@@ -1,9 +1,20 @@
 import styled from 'styled-components';
 import React from 'react';
 
-const Container = styled.div``;
-const Image = styled.img``;
-const Info = styled.div``;
+const Container = styled.div`
+  flex: 1; // images will grow to fill space.
+  margin: 3px; // images will have space between them.
+  height: 70vh;
+  position: relative;
+`;
+const Image = styled.img`
+  width: 100%;
+  height: 100%;
+  object-fit: cover; // crops squished images to fix aspect ration
+`;
+const Info = styled.div`
+  position: absolute;
+`;
 const Title = styled.h1``;
 const Button = styled.button``;
 
