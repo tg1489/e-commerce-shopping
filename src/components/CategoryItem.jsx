440612018c4832495cbd74b008363722c829b0de
Tony <tonyguarino1489@gmail.com>
2024-03-03 10:18:17 -0500

    Fixed styled import error

--- src/components/CategoryItem.jsx
+++ src/components/CategoryItem.jsx
@@ -1,8 +1,8 @@
-import { styled } from 'styled-components';
+import styled from 'styled-components';
 import React from 'react';
 
 const Container = styled.div``;
 
 export default function CategoryItem({ item }) {
-  return <Container></Container>;
+  return <Container>Hello</Container>;
 }
