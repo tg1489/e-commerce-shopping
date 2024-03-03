3bb2e20c4c075aa830c2ccd2fe63323ed4926ebc
Tony <tonyguarino1489@gmail.com>
2024-03-03 10:24:36 -0500

    Created styled components

--- src/components/CategoryItem.jsx
+++ src/components/CategoryItem.jsx
@@ -2,6 +2,10 @@ import styled from 'styled-components';
 import React from 'react';
 
 const Container = styled.div``;
+const Image = styled.img``;
+const Info = styled.div``;
+const Title = styled.h1``;
+const Button = styled.button``;
 
 export default function CategoryItem({ item }) {
   return (
