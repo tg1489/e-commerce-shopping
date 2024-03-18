7884d5f4ec880ca3901ce21be8387ed890c89e98
Tony <tonyguarino1489@gmail.com>
2024-03-18 17:54:31 -0400

    added styled divs to newsletter

--- src/components/Newsletter.jsx
+++ src/components/Newsletter.jsx
@@ -1,14 +1,24 @@
-import { Description } from '@mui/icons-material';
 import React from 'react';
+import styled from 'styled-components';
+import SendIcon from '@mui/icons-material/Send';
 
 export default function Newsletter() {
+  const Container = styled.div``;
+  const Desc = styled.div``;
+  const Title = styled.div``;
+  const InputContainer = styled.div``;
+  const Input = styled.input``;
+  const Button = styled.button``;
+
   return (
     <Container>
       <Title></Title>
-      <Description></Description>
+      <Desc></Desc>
       <InputContainer>
         <Input />
-        <Button></Button>
+        <Button>
+          <SendIcon />
+        </Button>
       </InputContainer>
     </Container>
   );
