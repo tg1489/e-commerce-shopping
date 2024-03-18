34aea6494c18b197b06057d042eebc410914b1c0
Tony <tonyguarino1489@gmail.com>
2024-03-18 17:58:38 -0400

    added newsletter and info to home

--- src/components/Newsletter.jsx
+++ src/components/Newsletter.jsx
@@ -3,19 +3,25 @@ import styled from 'styled-components';
 import SendIcon from '@mui/icons-material/Send';
 
 export default function Newsletter() {
-  const Container = styled.div``;
+  const Container = styled.div`
+    height: 60vh;
+    background-color: #fcf5f5;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  `;
   const Desc = styled.div``;
-  const Title = styled.div``;
+  const Title = styled.h1``;
   const InputContainer = styled.div``;
   const Input = styled.input``;
   const Button = styled.button``;
 
   return (
     <Container>
-      <Title></Title>
-      <Desc></Desc>
+      <Title>Newsletter</Title>
+      <Desc>Get timely updates from your favorite products.</Desc>
       <InputContainer>
-        <Input />
+        <Input placeholder='Your email' />
         <Button>
           <SendIcon />
         </Button>
