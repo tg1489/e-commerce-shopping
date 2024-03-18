0c54db5906d4a8b5b18d2f825449e3eb213e916e
Tony <tonyguarino1489@gmail.com>
2024-03-18 13:53:22 -0400

    added new product images to assets

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,6 +3,7 @@ import Navbar from '../components/Navbar';
 import Announcement from '../components/Announcement';
 import Slider from '../components/Slider';
 import Categories from '../components/Categories';
+import Products from '../components/Products';
 
 export default function Home() {
   return (
@@ -11,6 +12,7 @@ export default function Home() {
       <Navbar />
       <Slider />
       <Categories />
+      <Products />
     </div>
   );
 }
