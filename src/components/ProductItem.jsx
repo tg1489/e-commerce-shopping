d34f471aca6a2c6a7c2ce9a6e02ffd12d62758d0
Tony <tonyguarino1489@gmail.com>
2024-03-18 17:47:39 -0400

    added newsletter component

--- src/components/ProductItem.jsx
+++ src/components/ProductItem.jsx
@@ -4,6 +4,22 @@ import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 import SearchIcon from '@mui/icons-material/Search';
 import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
 
+const Info = styled.div`
+  opacity: 0;
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  top: 0;
+  left: 0;
+  background-color: rgba(0, 0, 0, 0.2);
+  z-index: 3;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  transition: all 0.5s ease;
+  cursor: pointer;
+`;
+
 const Container = styled.div`
   flex: 1;
   margin: 5px;
@@ -14,6 +30,10 @@ const Container = styled.div`
   justify-content: center;
   background-color: #f5fbfd;
   position: relative;
+
+  &:hover ${Info} {
+    opacity: 1; // Shows Info on hover
+  }
 `;
 
 const Circle = styled.div`
@@ -27,19 +47,7 @@ const Image = styled.img`
   height: 75%;
   z-index: 2;
 `;
-const Info = styled.div`
-  opacity: 0;
-  width: 100%;
-  height: 100%;
-  position: absolute;
-  top: 0;
-  left: 0;
-  background-color: rgba(0, 0, 0, 0.2);
-  z-index: 3;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-`;
+
 const Icon = styled.div`
   width: 40px;
   height: 40px;
