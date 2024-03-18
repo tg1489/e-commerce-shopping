ca5e1c37a0f3de99c50c07151701835b77767923
Tony <tonyguarino1489@gmail.com>
2024-03-18 18:13:41 -0400

    added footer with styles

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import styled from 'styled-components';
+
+export default function Footer() {
+  const Container = styled.div`
+    display: flex;
+  `;
+  const Left = styled.div``;
+  const Right = styled.div``;
+  const Center = styled.div``;
+
+  return <Container>Footer</Container>;
+}
