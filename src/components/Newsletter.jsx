7a3e4e814672544ca7facd332c199d1c434d49a9
Tony <tonyguarino1489@gmail.com>
2024-03-18 18:10:14 -0400

    Finished newsletter

--- src/components/Newsletter.jsx
+++ src/components/Newsletter.jsx
@@ -9,12 +9,36 @@ export default function Newsletter() {
     display: flex;
     align-items: center;
     justify-content: center;
+    flex-direction: column;
   `;
-  const Desc = styled.div``;
-  const Title = styled.h1``;
-  const InputContainer = styled.div``;
-  const Input = styled.input``;
-  const Button = styled.button``;
+  const Desc = styled.div`
+    font-size: 24px;
+    font-weight: 300;
+    margin-bottom: 20px;
+  `;
+  const Title = styled.h1`
+    font-size: 70px;
+    margin: 20px;
+  `;
+  const InputContainer = styled.div`
+    width: 50%;
+    height: 40px;
+    background-color: #fff;
+    display: flex;
+    justify-content: space-between;
+    border: 1px solid lightgray;
+  `;
+  const Input = styled.input`
+    border: none;
+    flex: 8;
+    padding-left: 20px;
+  `;
+  const Button = styled.button`
+    flex: 1;
+    border: none;
+    background-color: teal;
+    color: #fff;
+  `;
 
   return (
     <Container>
