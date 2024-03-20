e5dca9903f530f05e76a9b0abfd803678bac52b7
Tony <tonyguarino1489@gmail.com>
2024-03-20 18:05:59 -0400

    added all list items

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -72,7 +72,16 @@ export default function Footer() {
       <Center>
         <Title>Useful Links</Title>
         <List>
-          <ListItem></ListItem>
+          <ListItem>Home</ListItem>
+          <ListItem>Cart</ListItem>
+          <ListItem>Man Fashion</ListItem>
+          <ListItem>Woman Fashion</ListItem>
+          <ListItem>Accessories</ListItem>
+          <ListItem>My Account</ListItem>
+          <ListItem>Order Tracking</ListItem>
+          <ListItem>Wishlist</ListItem>
+          <ListItem>Wishlist</ListItem>
+          <ListItem>Terms</ListItem>
         </List>
       </Center>
       <Right></Right>
