29f2b4821c248cfe856a617008eadf3231d79d4f
Tony <tonyguarino1489@gmail.com>
2024-03-25 16:06:28 -0400

    added final style to select boxes

--- src/pages/ProductList.jsx
+++ src/pages/ProductList.jsx
@@ -24,9 +24,14 @@ const Filter = styled.div`
 const FilterText = styled.span`
   font-size: 20px;
   font-weight: 600;
+  margin-right: 20px;
 `;
 
-const Select = styled.select``;
+const Select = styled.select`
+  padding: 10px;
+  margin-right: 20px;
+`;
+
 const Option = styled.option``;
 
 export default function ProductList() {
