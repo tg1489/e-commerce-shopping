26fa5c45f12f84fa6cc06085e4f791bf0642f58d
Tony <tonyguarino1489@gmail.com>
2024-03-25 15:54:23 -0400

    added last 3 components

--- src/pages/ProductList.jsx
+++ src/pages/ProductList.jsx
@@ -2,6 +2,9 @@ import React from 'react';
 import styled from 'styled-components';
 import Navbar from '../components/Navbar';
 import Announcement from '../components/Announcement';
+import Products from '../components/Products';
+import Newsletter from '../components/Newsletter';
+import Footer from '../components/Footer';
 
 const Container = styled.div``;
 
@@ -28,6 +31,9 @@ export default function ProductList() {
         <Filter>Filter1</Filter>
         <Filter>Filter2</Filter>
       </FilterContainer>
+      <Products />
+      <Newsletter />
+      <Footer />
     </Container>
   );
 }
