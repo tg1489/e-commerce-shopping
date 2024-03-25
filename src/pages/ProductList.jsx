c5160b23489124d79ca6c4c8ffc729c5e2ed0e42
Tony <tonyguarino1489@gmail.com>
2024-03-25 15:41:45 -0400

    added announcement component

--- src/pages/ProductList.jsx
+++ src/pages/ProductList.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import styled from 'styled-components';
 import Navbar from '../components/Navbar';
+import Announcement from '../components/Announcement';
 
 const Container = styled.div``;
 
 export default function ProductList() {
   return (
     <Container>
+      <Announcement />
       <Navbar />
     </Container>
   );
