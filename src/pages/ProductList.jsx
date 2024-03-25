54ee683e90bf11f5998b1ad9e3305961ec32fa73
Tony <tonyguarino1489@gmail.com>
2024-03-25 15:47:35 -0400

    added margins to title and filter

--- src/pages/ProductList.jsx
+++ src/pages/ProductList.jsx
@@ -4,9 +4,19 @@ import Navbar from '../components/Navbar';
 import Announcement from '../components/Announcement';
 
 const Container = styled.div``;
-const Container = styled.div``;
-const Container = styled.div``;
-const Container = styled.div``;
+
+const Title = styled.h1`
+  margin: 20px;
+`;
+
+const FilterContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
+
+const Filter = styled.div`
+  margin: 20px;
+`;
 
 export default function ProductList() {
   return (
