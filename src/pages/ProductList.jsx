fe912cd89182a5b1b850993298558b8af1b95268
Tony <tonyguarino1489@gmail.com>
2024-03-25 16:04:20 -0400

    added options for sorting and filters

--- src/pages/ProductList.jsx
+++ src/pages/ProductList.jsx
@@ -26,6 +26,9 @@ const FilterText = styled.span`
   font-weight: 600;
 `;
 
+const Select = styled.select``;
+const Option = styled.option``;
+
 export default function ProductList() {
   return (
     <Container>
@@ -33,8 +36,38 @@ export default function ProductList() {
       <Navbar />
       <Title>Dresses</Title>
       <FilterContainer>
-        <Filter>Filter1</Filter>
-        <Filter>Filter2</Filter>
+        <Filter>
+          <FilterText>Filter Products:</FilterText>
+          <Select>
+            <Option disabled selected>
+              Color
+            </Option>
+            <Option>White</Option>
+            <Option>Black</Option>
+            <Option>Red</Option>
+            <Option>Blue</Option>
+            <Option>Yellow</Option>
+            <Option>Green</Option>
+          </Select>
+          <Select>
+            <Option disabled selected>
+              Size
+            </Option>
+            <Option>XS</Option>
+            <Option>S</Option>
+            <Option>M</Option>
+            <Option>L</Option>
+            <Option>XL</Option>
+          </Select>
+        </Filter>
+        <Filter>
+          <FilterText>Sort Products:</FilterText>
+          <Select>
+            <Option selected>Newest</Option>
+            <Option>Price (asc)</Option>
+            <Option>Price (desc)</Option>
+          </Select>
+        </Filter>
       </FilterContainer>
       <Products />
       <Newsletter />
