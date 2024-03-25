a4ebc248eaf598f31181ffef6289f79bbb4e8cf2
Tony <tonyguarino1489@gmail.com>
2024-03-25 15:31:59 -0400

    added navbar component to ProductList

--- src/pages/ProductList.jsx
+++ src/pages/ProductList.jsx
@@ -1,8 +1,13 @@
 import React from 'react';
 import styled from 'styled-components';
+import Navbar from '../components/Navbar';
 
 const Container = styled.div``;
 
 export default function ProductList() {
-  return <Container>Yes</Container>;
+  return (
+    <Container>
+      <Navbar />
+    </Container>
+  );
 }
