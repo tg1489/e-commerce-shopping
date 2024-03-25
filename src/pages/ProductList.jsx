4fc9041fa1227dc859924b77044819d792e3ffc8
Tony <tonyguarino1489@gmail.com>
2024-03-25 15:43:45 -0400

    added filters and title

--- src/pages/ProductList.jsx
+++ src/pages/ProductList.jsx
@@ -4,12 +4,20 @@ import Navbar from '../components/Navbar';
 import Announcement from '../components/Announcement';
 
 const Container = styled.div``;
+const Container = styled.div``;
+const Container = styled.div``;
+const Container = styled.div``;
 
 export default function ProductList() {
   return (
     <Container>
       <Announcement />
       <Navbar />
+      <Title>Dresses</Title>
+      <FilterContainer>
+        <Filter>Filter1</Filter>
+        <Filter>Filter2</Filter>
+      </FilterContainer>
     </Container>
   );
 }
