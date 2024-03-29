e51c4f4937691eb14f031ebfd0141052d6c04bf9
Tony <tonyguarino1489@gmail.com>
2024-03-29 10:34:33 -0400

    added necessary components

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -1,8 +1,19 @@
 import React from 'react';
 import styled from 'styled-components';
+import Announcement from '../components/Announcement';
+import Navbar from '../components/Navbar';
+import Newsletter from '../components/Newsletter';
+import Footer from '../components/Footer';
 
 const Container = styled.div``;
 
 export default function Product() {
-  return <Container>Product</Container>;
+  return (
+    <Container>
+      <Announcement />
+      <Navbar />
+      <Newsletter />
+      <Footer />
+    </Container>
+  );
 }
