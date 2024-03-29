135cb8efe6ba27947a820bc3611025e9d3640051
Tony <tonyguarino1489@gmail.com>
2024-03-29 10:40:46 -0400

    added infocontainer with components inside

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -16,7 +16,18 @@ export default function Product() {
         <ImgContainer>
           <Image src='https://i.ibb.co/S6qMxwr/jean.jpg' />
         </ImgContainer>
+        <InfoContainer>
+          <Title>Denim Jumpsuit</Title>
+          <Desc>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Adipisci
+            possimus quos tempore repellat ducimus dolores autem libero! Non
+            illum commodi a quam voluptatum libero vero architecto consequuntur
+            quae dolore. Voluptas.
+          </Desc>
+          <Price>$20</Price>
+        </InfoContainer>
       </Wrapper>
+
       <Newsletter />
       <Footer />
     </Container>
