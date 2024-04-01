3185536ac0611a920c90e340580f6f43b0ef9bce
Tony <tonyguarino1489@gmail.com>
2024-04-01 15:07:13 -0400

    Fixed image and info container to fit properly

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -6,10 +6,32 @@ import Newsletter from '../components/Newsletter';
 import Footer from '../components/Footer';
 
 const Container = styled.div``;
-const Wrapper = styled.div``;
-const ImgContainer = styled.div``;
-const Image = styled.div``;
-const InfoContainer = styled.div``;
+
+const Wrapper = styled.div`
+  padding: 50px;
+  display: flex;
+`;
+
+const ImgContainer = styled.div`
+  flex: 1;
+`;
+
+const Image = styled.img`
+  width: 100%;
+  height: 90vh;
+  object-fit: cover;
+`;
+
+const InfoContainer = styled.div`
+  flex: 1;
+  padding: 0px 50px;
+`;
+
+const Title = styled.h1``;
+
+const Desc = styled.p``;
+
+const Price = styled.span``;
 
 export default function Product() {
   return (
