47bf69b2e01e8a0f613df23bcdd33d7f03b42ae6
Tony <tonyguarino1489@gmail.com>
2024-04-02 15:23:39 -0400

    finished filtersize style

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -66,7 +66,10 @@ const FilterColor = styled.div`
   cursor: pointer;
 `;
 
-const FilterSize = styled.select``;
+const FilterSize = styled.select`
+  margin-left: 10px;
+  padding: 5px;
+`;
 
 const FilterSizeOption = styled.option``;
 
