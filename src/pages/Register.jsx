8be5058b1324e37063f9e97c5f04c59dff64bf94
Tony <tonyguarino1489@gmail.com>
2024-04-03 17:02:58 -0400

    added agreement and button

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -11,11 +11,17 @@ export default function Register() {
       <Wrapper>
         <Title>CREATE AN ACCOUNT</Title>
         <Form>
-          <Input placeholder='name' />
+          <Input placeholder='first name' />
           <Input placeholder='last name' />
           <Input placeholder='email' />
           <Input placeholder='username' />
           <Input placeholder='password' />
+          <Input placeholder='confirm password' />
+          <Agreement>
+            By creating an account, I consent to the processing of my personal
+            data in accordance with <b>PRIVACY POLICY</b>
+          </Agreement>
+          <Button>CREATE</Button>
         </Form>
       </Wrapper>
     </Container>
