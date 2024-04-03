d23221b992859b88c0d25c76312ed83f040a2dd0
Tony <tonyguarino1489@gmail.com>
2024-04-03 17:06:45 -0400

    added width and height to container

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,9 +1,23 @@
 import React from 'react';
 import styled from 'styled-components';
 
-const Container = styled.div``;
+const Container = styled.div`
+  width: 100vw;
+  height: 100vh;
+  
+`;
+
 const Wrapper = styled.div``;
+
 const Title = styled.h1``;
+
+const Form = styled.form``;
+
+const Input = styled.input``;
+
+const Agreement = styled.span``;
+
+const Button = styled.button``;
 
 export default function Register() {
   return (
