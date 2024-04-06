10fe48ed76f9e0ca11f6177cf89cb09db839dd3d
Tony <tonyguarino1489@gmail.com>
2024-04-06 11:22:52 -0400

    added login data

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -4,5 +4,19 @@ import styled from 'styled-components';
 const Container = styled.div``;
 
 export default function Login() {
-  return <Container>Login</Container>;
+  return (
+    <Container>
+      <Wrapper>
+        <Title>SIGN IN</Title>
+        <Form>
+          <Input placeholder='username' />
+          <Input placeholder='password' />
+
+          <Button>LOGIN</Button>
+          <Link>FORGOT PASSWORD?</Link>
+          <Link>CREATE A NEW ACCOUNT</Link>
+        </Form>
+      </Wrapper>
+    </Container>
+  );
 }
