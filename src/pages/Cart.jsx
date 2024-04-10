3029c909e9d9eedf1371dd70e9e6c16f57098d1a
Tony <tonyguarino1489@gmail.com>
2024-04-09 20:31:57 -0400

    Added new components to cart with title

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -2,14 +2,22 @@ import React from 'react';
 import styled from 'styled-components';
 import Navbar from '../components/Navbar';
 import Announcement from '../components/Announcement';
+import Footer from '../components/Footer';
 
 const Container = styled.div``;
+const Wrapper = styled.div``;
+const Title = styled.h1``;
 
 export default function Cart() {
   return (
     <Container>
       <Announcement />
       <Navbar />
+      <Wrapper>
+        <Title>YOUR BAG</Title>
+      </Wrapper>
+
+      <Footer />
     </Container>
   );
 }
