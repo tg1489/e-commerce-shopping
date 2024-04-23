65ee5281ab9c922e10bc94748384014ce8e01dae
Tony <tonyguarino1489@gmail.com>
2024-04-23 11:57:14 -0400

    Changed flex direction for categories on mobile

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 import styled from 'styled-components';
 import { categories } from '../utils/data';
 import CategoryItem from './CategoryItem';
+import { mobile } from '../utils/responsive';
 
 const Container = styled.div`
   display: flex;
   padding: 20px;
   justify-content: space-between;
+  ${mobile({ padding: '0px', flexDirection: 'column' })}
 `;
 
 export default function Categories() {
